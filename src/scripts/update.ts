9df172570237be0fdb44a54873cc82b0d7540f21
kirillsaint <kirillsaint@wtfuck.tk>
2024-10-12 17:34:37 +0600

    remove update script

--- src/scripts/update.ts
+++ src/scripts/update.ts
@@ -1,22 +1,3 @@
 /**
  * Скрипт, который можно запустить при обновлении серверов
  */
-import { exec } from "child_process";
-import util from "util";
-
-const execAsync = util.promisify(exec);
-
-export async function disablePorts(execFunc: (command: string) => void) {
-	await execFunc("sudo ufw default allow incoming");
-	await execFunc("sudo ufw default allow outgoing");
-	await execFunc("sudo ufw deny 25");
-	await execFunc("sudo ufw deny 465");
-	await execFunc("sudo ufw deny 587");
-	await execFunc("sudo ufw --force enable");
-}
-
-disablePorts(async (command: string) => {
-	console.log(command);
-	const data = await execAsync(command);
-	console.log(data);
-});
