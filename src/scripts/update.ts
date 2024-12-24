92f06185ec2801e4647914e6c3d41f6b331861e4
Кирилл Персонов <kirillsaint@wtfuck.tk>
2024-12-24 21:57:05 +0600

    пока апдейт скрипт

--- src/scripts/update.ts
+++ src/scripts/update.ts
@@ -1,17 +1,3 @@
 /**
  * Скрипт, который можно запустить при обновлении серверов
  */
-import { exec } from "child_process";
-import util from "util";
-
-const execAsync = util.promisify(exec);
-
-async () => {
-	const blacklist = [6881, 6882, 6883, 6884, 6885, 6886, 6887, 6888, 6889];
-
-	for (const port of blacklist) {
-		try {
-			await execAsync(`sudo ufw deny ${port}`);
-		} catch (error) {}
-	}
-};
